7e3cc9a70120c103e5d0f23ddaf7c58e6b1f172b
David <david318107680@gmail.com>
2026-05-20 13:18:44 -0600

    Actualización completa del proyecto

--- src/data/users.js
+++ src/data/users.js
@@ -1,6 +1,6 @@
 export const usuariosPermitidos = {
-  "Tania": { pin: "tania", rol: "Taquilla" },
+  "Tania": { pin: "3128", rol: "Taquilla" },
   "Monse": { pin: "3312", rol: "Cafeteria" },
   "Yunn": { pin: "2701", rol: "Admin" },
-  "Sandy": { pin: "0312", rol: "Admin" }
+  "Sandy": { pin: "yaili", rol: "Admin" }
 };
