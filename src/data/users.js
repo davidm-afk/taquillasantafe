d6b809f99dc1bfd83c869d1770d4178281bf4ef8
davidm-afk <david318107680@gmail.com>
2026-06-12 15:50:16 -0600

    cambio contraseñas

--- src/data/users.js
+++ src/data/users.js
@@ -1,8 +1,8 @@
 export const usuariosPermitidos = {
   "Tania": { pin: "3128", rol: "Taquilla" },
   "Monse": { pin: "3312", rol: "Cafeteria" },
-  "Yunn": { pin: "2510", rol: "Admin" },
-  "Sandy": { pin: "ket03", rol: "Admin" },
+  "Yunn": { pin: "1211", rol: "Admin" },
+  "Sandy": { pin: "MAA23", rol: "Admin" },
   "Fernanda": { pin: "0108", rol: "Eventos" },
   "Brigitte": { pin: "1606", rol: "Eventos" }
 };
